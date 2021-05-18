bcd5af75e8e710b2cebb0db08f65b7768a5b6123
MikhaZyu <MikhaZyu@gmail.com>
2021-05-18 18:31:24 +0300

    Experiments

---
+++ IntegrationExperiments/Client/Pages/WorkflowExamplePage.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using IntegrationExperiments.Shared;
+
+namespace IntegrationExperiments.Client.Pages
+{
+    public partial class WorkflowExamplePage: ComponentBase
+    {
+        private List<string> _responses = new List<string>();
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        private async Task OnBtnClick()
+        {
+
+            var responseDto = await Http.GetFromJsonAsync<ResponseDto<string>>("WorkflowRun/Ping");
+
+            _responses.Add(responseDto?.Data);
+        }
+    }
+}
---
+++ IntegrationExperiments/Server/Controllers/WorkflowRunController.cs
@@ -0,0 +1,28 @@
+﻿using IntegrationExperiments.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationExperiments.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WorkflowRunController : ControllerBase
+    {
+        [HttpGet("Ping")]
+        public ResponseDto<string> Ping()
+        {
+            var responseData = new ResponseDto<string>()
+            {
+                Status = 200,
+                Message = "OK",
+                Data = $"OK-{DateTime.Now}"
+            };
+
+            return responseData;
+        }
+    }
+}
